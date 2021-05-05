51093f0fcd1c7a616df0cb312e21844662a131ba
Jeremy <jeremy.perrouault@ascadis.fr>
2021-05-05 12:30:03 +0200

    SQL: Insert

--- formation-java-2/src/main/java/fr/formation/ApplicationSql.java
+++ formation-java-2/src/main/java/fr/formation/ApplicationSql.java
@@ -1,7 +1,9 @@
 package fr.formation;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -30,6 +32,39 @@ public class ApplicationSql {
 			while (resultSet.next()) { //Tant qu'on a un résultat
 				System.out.println(resultSet.getString("PRO_LIBELLE"));
 			}
+			
+			//INSERT
+			String produitLibelle = "FROM JAVA (v1)";
+			String produitLibellev2 = "FROM JAVA (v2)";
+			BigDecimal produitPrix = new BigDecimal(100);
+			int produitStock = 4;
+			BigDecimal produitPrixAchat = new BigDecimal(50);
+			int produitFournisseurId = 1;
+			int produitCategorieId = 1;
+			
+			//Version "classique", la concaténation de chaine (!!! attention injection sql ... !!!)
+			statement.execute("INSERT INTO produit (PRO_LIBELLE, PRO_PRIX, PRO_STOCK, PRO_PRIX_ACHAT, PRO_FOURNISSEUR_ID, PRO_CATEGORIE_ID) " + 
+					"	VALUES ('" + produitLibelle + "', " + produitPrix + ", " + produitStock + ", " + produitPrixAchat + ", " + produitFournisseurId + ", " + produitCategorieId + ")");
+			
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO produit (PRO_LIBELLE, PRO_PRIX, PRO_STOCK, PRO_PRIX_ACHAT, PRO_FOURNISSEUR_ID, PRO_CATEGORIE_ID) VALUES (?, ?, ?, ?, ?, ?)");
+			
+			if (true) {
+				preparedStatement.setString(1, produitLibellev2);
+			}
+			
+			else {
+				preparedStatement.setString(1, produitLibelle);
+			}
+			
+			preparedStatement.setBigDecimal(2, produitPrix);
+			preparedStatement.setInt(3, produitStock);
+			preparedStatement.setBigDecimal(4, produitPrixAchat);
+			preparedStatement.setInt(5, produitFournisseurId);
+			preparedStatement.setInt(6, produitCategorieId);
+			
+			//ON EXECUTE LA REQUETE
+			int rows = preparedStatement.executeUpdate();
+			System.out.println(rows + " lignes enregistrées.");
 		}
 		
 		catch (SQLException sqle) {
