1199cbd736658a3a58d038505b4e1b73e5dcc277
Jeremy <jeremy.perrouault@ascadis.fr>
2021-05-10 14:28:58 +0200

    Démo JPQL en JAVA

--- eshop-java/src/main/java/fr/formation/TestHibernateApplication.java
+++ eshop-java/src/main/java/fr/formation/TestHibernateApplication.java
@@ -1,8 +1,13 @@
 package fr.formation;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import fr.formation.model.Fournisseur;
+import fr.formation.model.Produit;
 
 public class TestHibernateApplication {
 	public static void main(String[] args) {
@@ -13,6 +18,39 @@ public class TestHibernateApplication {
 		//Récupérer un EntityManager
 		EntityManager em = emf.createEntityManager();
 		
+		Produit monProduit = em.find(Produit.class, 1);
+		System.out.println(monProduit.getLibelle());
+		System.out.println(monProduit.getCategorie().getLibelle());
+		
+		List<Produit> mesProduits = em
+			.createQuery("select p from Produit p", Produit.class)
+			.getResultList();
+		
+		for (Produit p : mesProduits) {
+			System.out.println(p.getId() + " - " + p.getLibelle());
+		}
+		
+		List<Produit> mesProduits2 = em
+//			.createQuery("select p from Produit p where p.fournisseur.id = :fournisseurId", Produit.class)
+			.createQuery("select p from Produit p where p.fournisseur.id = ?1", Produit.class)
+//			.setParameter("fournisseurId", 1)
+			.setParameter(1, 1)
+			.getResultList();
+		
+		for (Produit p : mesProduits2) {
+			System.out.println(p.getId() + " - " + p.getLibelle());
+		}
+		
+		//fetch : permet de rattacher la liste des produits au fournisseur
+		List<Fournisseur> mesFournisseurs = em
+			.createQuery("select distinct f from Fournisseur f left join fetch f.produits", Fournisseur.class)
+			.getResultList();
+		
+		em.close();
+		
+		for (Fournisseur f : mesFournisseurs) {
+			System.out.println(f.getId() + " - " + f.getNom() + ", " + f.getProduits().size());
+		}
 		
 		//Fermer EntityManagerFactory, pour fermer les connexions avec SQL etc.
 		emf.close();
