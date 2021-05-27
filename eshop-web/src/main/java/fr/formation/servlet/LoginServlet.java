47115e4890b4c0aa3a0a10536e53b6b59a01c1cd
Jeremy <jeremy.perrouault@ascadis.fr>
2021-05-27 12:56:49 +0200

    SERVLET/JSP - JOUR 1 - PREMIERE PARTIE EXO LOGIN

---
+++ eshop-web/src/main/java/fr/formation/servlet/LoginServlet.java
@@ -0,0 +1,44 @@
+package fr.formation.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	//Affichage du formulaire de connexion
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext()
+			.getRequestDispatcher("/WEB-INF/login.jsp")
+			.forward(req, resp);
+	}
+	
+	
+	//Traitement de la connexion
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String myUsername = req.getParameter("username");
+		String myPassword = req.getParameter("password");
+
+//		System.out.println(myUsername);
+//		System.out.println(myPassword);
+		
+		//Vérifier le login & password
+		if (myUsername.equals("toto") && myPassword.equals("titi")) {
+			//Enregistrer le username dans la session utilisateur
+			
+			//Rediriger vers /home
+			resp.sendRedirect("home");
+		}
+		
+		else {
+			//Rediriger vers /login
+			resp.sendRedirect("login");
+		}
+	}
+}
---
+++ eshop-web/src/main/webapp/WEB-INF/login.jsp
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Connexion</title>
+	</head>
+	
+	<body>
+		<h1>Se connecter</h1>
+		
+		<form method="POST">
+			<div>
+				<label>Nom d'utilisateur</label>
+				<input type="text" name="username" />
+			</div>
+			
+			<div>
+				<label>Mot de passe</label>
+				<input type="password" name="password" />
+			</div>
+			
+			<div>
+				<input type="submit" value="Se connecter" />
+			</div>
+		</form>
+	</body>
+</html>
