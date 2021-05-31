3b0ac74c0547ac3832b6aa12ee63abf244e13194
Jeremy <jeremy.perrouault@ascadis.fr>
2021-05-31 11:32:24 +0200

    SERVLET/JSP - JOUR 3 - Listener Applicatif instanciation DAO

---
+++ eshop-web/src/main/java/fr/formation/listener/ContextListener.java
@@ -0,0 +1,22 @@
+package fr.formation.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import fr.formation.dao.DAOFactory;
+import fr.formation.service.ProduitService;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("Initialisation du contexte des DAO ...");
+		
+		//On instancie toutes nos DAO (soit en créant le service, soit en appelant la Factory)
+		new ProduitService();
+		DAOFactory.createFournisseurDao();
+		DAOFactory.createCategoryDao();
+		DAOFactory.createAttributDao();
+	}
+}
