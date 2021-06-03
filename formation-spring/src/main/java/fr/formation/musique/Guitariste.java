07bf4a59cf9bb57f0bb97c8e91228e55497bf5c5
Jeremy <jeremy.perrouault@ascadis.fr>
2021-06-03 11:30:15 +0200

    SPRING CORE : JOUR 1 - Création contexte SPRING (xml & déclaration java)

--- formation-spring/src/main/java/fr/formation/musique/Guitariste.java
+++ formation-spring/src/main/java/fr/formation/musique/Guitariste.java
@@ -1,6 +1,14 @@
 package fr.formation.musique;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Guitariste implements IMusicien {
+//	@Autowired(required = false)
+	@Autowired
+	@Qualifier("guitare")
 	private IInstrument instrument;
 
 	public IInstrument getInstrument() {
