0c5a9c2723b63e6c4a487fd166c53103041bb000
Jeremy <jeremy.perrouault@ascadis.fr>
2021-06-03 16:56:36 +0200

    SPRING CORE : JOUR 1 - INIT JPA CONFIG + SERVICE + APPLICATION

---
+++ eshop-java/src/main/java/fr/formation/ApplicationSpring.java
@@ -0,0 +1,28 @@
+package fr.formation;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import fr.formation.config.AppConfig;
+import fr.formation.model.Produit;
+import fr.formation.service.ProduitService;
+
+public class ApplicationSpring {
+	public static void main(String[] args) {
+		//Chargement du contexte de SPRING
+		AnnotationConfigApplicationContext myContext =
+				new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		//Récupérer le service ProduitService
+		ProduitService srvProduit = myContext.getBean(ProduitService.class);
+		
+		//Manipuler le service pour récupérer la liste des produits
+		List<Produit> mesProduits = srvProduit.findAll();
+		
+		//Parcours de la liste
+		for (Produit produit : mesProduits) {
+			System.out.println(produit.getLibelle());
+		}
+	}
+}
