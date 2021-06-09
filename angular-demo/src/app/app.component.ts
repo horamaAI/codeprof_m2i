2ab3a559c674031e4cedccbdaa0b06dfbb03db2e
Jeremy <jeremy.perrouault@ascadis.fr>
2021-06-09 14:44:11 +0200

    JOUR 4 - Demo ANGULAR

--- angular-demo/src/app/app.component.ts
+++ angular-demo/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-demo';
+  produits = [
+    { id: 1, libelle: "toto" },
+    { id: 2, libelle: "toto 2" }
+  ];
+
+  async ngOnInit() {
+    this.produits = await fetch("http://localhost:8080/eshop-web/api/produit")
+                          .then(resp => resp.json());
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  async test() {
+    this.produits = await fetch("http://localhost:8080/eshop-web/api/produit")
+                          .then(resp => resp.json());
+  }
 }
