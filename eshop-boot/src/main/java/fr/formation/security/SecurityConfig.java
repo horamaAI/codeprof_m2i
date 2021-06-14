6ac668de50f8ea63b98424bcd5aab10ca7efc20f
Jeremy <jeremy.perrouault@ascadis.fr>
2021-06-14 11:00:53 +0200

    JOUR 6 - DB USER SPRING SECURITY

--- eshop-boot/src/main/java/fr/formation/security/SecurityConfig.java
+++ eshop-boot/src/main/java/fr/formation/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package fr.formation.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -30,9 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			;
 	}
 	
-	@Override //CONFIGURATION DES USERS
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user").password("{noop}123456").roles("USER");
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+	
+	
+	// SPRING BOOT CAPTE ET GERE LA CLASSE AuthService @Service
+//	@Override //CONFIGURATION DES USERS
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//			.withUser("user").password("{noop}123456").roles("USER");
+//	}
 }
---
+++ eshop-boot/src/main/java/fr/formation/security/UtilisateurPrincipal.java
@@ -0,0 +1,64 @@
+package fr.formation.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import fr.formation.model.Utilisateur;
+
+public class UtilisateurPrincipal implements UserDetails {
+	private Utilisateur utilisateur;
+	
+	public UtilisateurPrincipal(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		if (this.utilisateur.isAdmin()) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+		
+		else {
+			authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		}
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.utilisateur.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.utilisateur.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
